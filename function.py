3bee14a9feda826cef1989d1fd86d6a9c3af4c90
idir <idir.ait@gmail.com>
2024-11-05 18:46:19 +0100

    save

--- function.py
+++ function.py
@@ -1,4 +1,6 @@
 
+import yfinance as yf
+
 x = 20
 
 def coucou(a: int)-> str:
@@ -12,3 +14,21 @@ def getx(name):
 
 def badfun(name):
     return not_exist_var 
+
+# https://localhost:5000/api/call/getHistory?args={"symbol":"MSFT"}
+def getHistory(symbol: str)-> str:
+    
+    ticker = yf.Ticker(symbol)
+
+    # get all stock info
+    #ticker.info
+
+    # get historical market data
+    hist = ticker.history(period="1mo")
+
+    hist.index = hist.index.tz_convert(None)
+    #hist.to_parquet("data/table",  engine='fastparquet', partition_cols=["Date"])
+    hist.to_parquet("data/table",  engine='fastparquet', partition_cols=["Date"], append=True)
+    return str(hist)
+
+print(getHistory("MSFT"))
