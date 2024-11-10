f39c24e849955353e2ff2704e9965d02d745651b
idir <idir.ait@gmail.com>
2024-11-10 21:17:36 +0100

    save

--- endpoints.py
+++ endpoints.py
@@ -1,23 +1,29 @@
-
+import os
 import json
+import datetime
 import yfinance as yf
 import pandas as pd
-import datetime
-import os
 from pprint import pprint
+
 
 DATA_PATH = "data"
 
 
 def is_empty_dir(path):
-    if os.path.isdir(path) and os.listdir(path):
-        return False
+
+    if os.path.isdir(path):
+        files = os.listdir(path)
+        if '_common_metadata' in files:
+            files.remove('_common_metadata')
+        if '_metadata' in files:
+            files.remove('_metadata')
+        if len(files) > 0:
+            return False
     return True
 
+
 # https://localhost:5000/api/call/load_stock?symbol="MSFT"
-
-
-def load_stock(symbol: str = "MSFT") -> str:
+def load_stock(symbol: str = "MSFT"):
 
     path = f"{DATA_PATH}/stocks/{symbol}"
 
@@ -36,45 +42,43 @@ def load_stock(symbol: str = "MSFT") -> str:
             path, engine='fastparquet', columns=["Date"])["Date"].max()
         append_mode = True
 
-    hist = ticker.history(start=last_update.strftime('%Y-%m-%d'))
-    hist = hist[hist.index > last_update.strftime('%Y-%m-%d')]
+    data = ticker.history(start=last_update.strftime('%Y-%m-%d'))
+    data = data[data.index > last_update.strftime('%Y-%m-%d')]
     print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
-    print(f"Number new lines: {str(hist.shape[0])}")
+    print(f"Number new lines: {str(data.shape[0])}")
 
-    hist['partition'] = hist.index
-    hist['partition'] = pd.Categorical(hist['partition'].dt.strftime('%Y-%m'))
-    hist.reset_index(drop=False, inplace=True)
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
 
-    hist.to_parquet(path,  engine='fastparquet', partition_cols=[
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
                     "partition"], append=append_mode)
-
-    return True
 
 
 # https://localhost:5000/api/call/stock?symbol="MSFT"&start="2024-11-05"
 def stock(symbol: str = "MSFT",
           start: str = "1900-01-01",
-          end: str = "3000-01-01") -> str:
+          end: str = "3000-01-01") -> pd.core.frame.DataFrame:
 
     path = f"{DATA_PATH}/stocks/{symbol}"
-    stock = pd.read_parquet(path, engine='fastparquet')
-    stock.set_index("Date", inplace=True)
+    data = pd.read_parquet(path, engine='fastparquet')
+    data.set_index("Date", inplace=True)
 
-    stock = stock[(stock.index >= start) & (stock.index <= end)]
+    data = data[(data.index >= start) & (data.index <= end)]
 
-    stock.reset_index(drop=False, inplace=True)
-    stock["Date"] = stock["Date"].dt.strftime('%Y-%m-%d')
-    return stock.to_dict('list')
+    data.reset_index(drop=False, inplace=True)
+    data["Date"] = data["Date"].dt.strftime('%Y-%m-%d')
+    return data
 
 
 # https://localhost:5000/api/call/load_dividends?symbol="MSFT"
-def load_dividends(symbol: str = "MSFT") -> str:
+def load_dividends(symbol: str = "MSFT"):
 
     path = f"{DATA_PATH}/dividends/{symbol}"
     print(f"Loanding dividends : {symbol}")
     ticket = yf.Ticker(symbol)
 
-    df_div = ticket.dividends.to_frame()
+    data = ticket.dividends.to_frame()
 
     if is_empty_dir(path):
         last_update = pd.Timestamp(datetime.datetime(1970, 1, 1, 0, 0))
@@ -85,41 +89,39 @@ def load_dividends(symbol: str = "MSFT") -> str:
             path, engine='fastparquet', columns=["Date"])["Date"].max()
         append_mode = True
 
-    df_div = df_div[df_div.index > last_update.strftime('%Y-%m-%d')]
+    data = data[data.index > last_update.strftime('%Y-%m-%d')]
     print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
-    print(f"Number new lines: {str(df_div.shape[0])}")
+    print(f"Number new lines: {str(data.shape[0])}")
 
-    df_div['partition'] = df_div.index
-    df_div['partition'] = pd.Categorical(
-        df_div['partition'].dt.strftime('%Y-%m'))
-    df_div.reset_index(drop=False, inplace=True)
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(
+        data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
 
-    df_div.to_parquet(path,  engine='fastparquet', partition_cols=[
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
         "partition"], append=append_mode)
-
-    return True
 
 
 # https://localhost:5000/api/call/dividends?symbol="MSFT"
-def dividends(symbol: str = "MSFT") -> str:
+def dividends(symbol: str = "MSFT") -> pd.core.frame.DataFrame:
 
     path = f"{DATA_PATH}/dividends/{symbol}"
-    df_div = pd.read_parquet(path, engine='fastparquet')
-    df_div.set_index("Date", inplace=True)
+    data = pd.read_parquet(path, engine='fastparquet')
+    data.set_index("Date", inplace=True)
 
-    df_div.reset_index(drop=False, inplace=True)
-    df_div["Date"] = df_div["Date"].dt.strftime('%Y-%m-%d')
-    return df_div.to_dict('list')
+    data.reset_index(drop=False, inplace=True)
+    data["Date"] = data["Date"].dt.strftime('%Y-%m-%d')
+    return data
 
 
 # https://localhost:5000/api/call/load_splits?symbol="MSFT"
-def load_splits(symbol: str = "MSFT") -> str:
+def load_splits(symbol: str = "MSFT"):
 
     path = f"{DATA_PATH}/splits/{symbol}"
     print(f"Loanding splits : {symbol}")
     ticket = yf.Ticker(symbol)
 
-    df_div = ticket.splits.to_frame()
+    data = ticket.splits.to_frame()
 
     if is_empty_dir(path):
         last_update = pd.Timestamp(datetime.datetime(1970, 1, 1, 0, 0))
@@ -130,42 +132,40 @@ def load_splits(symbol: str = "MSFT") -> str:
             path, engine='fastparquet', columns=["Date"])["Date"].max()
         append_mode = True
 
-    df_div = df_div[df_div.index > last_update.strftime('%Y-%m-%d')]
+    data = data[data.index > last_update.strftime('%Y-%m-%d')]
     print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
-    print(f"Number new lines: {str(df_div.shape[0])}")
+    print(f"Number new lines: {str(data.shape[0])}")
 
-    df_div['partition'] = df_div.index
-    df_div['partition'] = pd.Categorical(
-        df_div['partition'].dt.strftime('%Y-%m'))
-    df_div.reset_index(drop=False, inplace=True)
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(
+        data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
 
-    df_div.to_parquet(path,  engine='fastparquet', partition_cols=[
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
         "partition"], append=append_mode)
-
-    return True
 
 
 # https://localhost:5000/api/call/splits?symbol="MSFT"
-def splits(symbol: str = "MSFT") -> str:
+def splits(symbol: str = "MSFT") -> pd.core.frame.DataFrame:
 
     path = f"{DATA_PATH}/splits/{symbol}"
-    df_div = pd.read_parquet(path, engine='fastparquet')
-    df_div.set_index("Date", inplace=True)
+    data = pd.read_parquet(path, engine='fastparquet')
+    data.set_index("Date", inplace=True)
 
-    df_div.reset_index(drop=False, inplace=True)
-    df_div["Date"] = df_div["Date"].dt.strftime('%Y-%m-%d')
-    return df_div.to_dict('list')
+    data.reset_index(drop=False, inplace=True)
+    data["Date"] = data["Date"].dt.strftime('%Y-%m-%d')
+    return data
 
 
 # https://localhost:5000/api/call/load_income?symbol="MSFT"
-def load_income(symbol: str = "MSFT") -> str:
+def load_income(symbol: str = "MSFT"):
 
     path = f"{DATA_PATH}/income_stmt/{symbol}"
     print(f"Loanding income_stmt : {symbol}")
     ticket = yf.Ticker(symbol)
 
-    df_income = ticket.income_stmt.transpose()
-    df_income.index.names = ['Date']
+    data = ticket.income_stmt.transpose()
+    data.index.names = ['Date']
 
     if is_empty_dir(path):
         last_update = pd.Timestamp(datetime.datetime(1970, 1, 1, 0, 0))
@@ -176,43 +176,40 @@ def load_income(symbol: str = "MSFT") -> str:
             path, engine='fastparquet', columns=["Date"])["Date"].max()
         append_mode = True
 
-    df_income = df_income[df_income.index > last_update.strftime('%Y-%m-%d')]
+    data = data[data.index > last_update.strftime('%Y-%m-%d')]
     print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
-    print(f"Number new lines: {str(df_income.shape[0])}")
+    print(f"Number new lines: {str(data.shape[0])}")
 
-    df_income['partition'] = df_income.index
-    df_income['partition'] = pd.Categorical(
-        df_income['partition'].dt.strftime('%Y-%m'))
-    df_income.reset_index(drop=False, inplace=True)
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(
+        data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
 
-    df_income.to_parquet(path,  engine='fastparquet', partition_cols=[
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
         "partition"], append=append_mode)
 
-    return True
 
 # https://localhost:5000/api/call/income?symbol="MSFT"
-
-
-def income(symbol: str = "MSFT") -> str:
+def income(symbol: str = "MSFT") -> pd.core.frame.DataFrame:
 
     path = f"{DATA_PATH}/income_stmt/{symbol}"
-    df_income = pd.read_parquet(path, engine='fastparquet')
-    df_income.set_index("Date", inplace=True)
+    data = pd.read_parquet(path, engine='fastparquet')
+    data.set_index("Date", inplace=True)
 
-    df_income.reset_index(drop=False, inplace=True)
-    df_income["Date"] = df_income["Date"].dt.strftime('%Y-%m-%d')
-    return df_income.to_dict('list')
+    data.reset_index(drop=False, inplace=True)
+    data["Date"] = data["Date"].dt.strftime('%Y-%m-%d')
+    return data
 
 
 # https://localhost:5000/api/call/load_quarterly_income?symbol="MSFT"
-def load_quarterly_income(symbol: str = "MSFT") -> str:
+def load_quarterly_income(symbol: str = "MSFT"):
 
     path = f"{DATA_PATH}/quarterly_income_stmt/{symbol}"
     print(f"Loanding quarterly_income_stmt : {symbol}")
     ticket = yf.Ticker(symbol)
 
-    df_quarterly_income = ticket.quarterly_income_stmt.transpose()
-    df_quarterly_income.index.names = ['Date']
+    data = ticket.quarterly_income_stmt.transpose()
+    data.index.names = ['Date']
 
     if is_empty_dir(path):
         last_update = pd.Timestamp(datetime.datetime(1970, 1, 1, 0, 0))
@@ -223,40 +220,133 @@ def load_quarterly_income(symbol: str = "MSFT") -> str:
             path, engine='fastparquet', columns=["Date"])["Date"].max()
         append_mode = True
 
-    df_quarterly_income = df_quarterly_income[df_quarterly_income.index > last_update.strftime(
+    data = data[data.index > last_update.strftime(
         '%Y-%m-%d')]
     print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
-    print(f"Number new lines: {str(df_quarterly_income.shape[0])}")
+    print(f"Number new lines: {str(data.shape[0])}")
 
-    df_quarterly_income['partition'] = df_quarterly_income.index
-    df_quarterly_income['partition'] = pd.Categorical(
-        df_quarterly_income['partition'].dt.strftime('%Y-%m'))
-    df_quarterly_income.reset_index(drop=False, inplace=True)
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(
+        data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
 
-    df_quarterly_income.to_parquet(path,  engine='fastparquet', partition_cols=[
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
         "partition"], append=append_mode)
-
-    return True
 
 
 # https://localhost:5000/api/call/quarterly_income?symbol="MSFT"
 def quarterly_income(symbol: str = "MSFT") -> pd.core.frame.DataFrame:
 
     path = f"{DATA_PATH}/quarterly_income_stmt/{symbol}"
-    df_quarterly_income = pd.read_parquet(path, engine='fastparquet')
-    df_quarterly_income.set_index("Date", inplace=True)
+    data = pd.read_parquet(path, engine='fastparquet')
+    data.set_index("Date", inplace=True)
 
-    df_quarterly_income.reset_index(drop=False, inplace=True)
-    df_quarterly_income["Date"] = df_quarterly_income["Date"].dt.strftime(
+    data.reset_index(drop=False, inplace=True)
+    data["Date"] = data["Date"].dt.strftime(
         '%Y-%m-%d')
-    return df_quarterly_income.to_dict('list')
+    return data
 
 # https://localhost:5000/api/call/SP500
-def SP500()-> pd.core.frame.DataFrame:
-    list_sp500 = pd.read_csv('https://datahub.io/core/s-and-p-500-companies/_r/-/data/constituents.csv')
+
+
+def SP500() -> pd.core.frame.DataFrame:
+
+    list_sp500 = pd.read_csv(
+        'https://datahub.io/core/s-and-p-500-companies/_r/-/data/constituents.csv')
     list_sp500['Symbol'] = list_sp500['Symbol'].str.replace('.', '-')
     return list_sp500
 
+
+# https://localhost:5000/api/call/load_cashflow?symbol="MSFT"
+def load_cashflow(symbol: str = "MSFT"):
+
+    path = f"{DATA_PATH}/cashflow/{symbol}"
+    print(f"Loanding cashflow : {symbol}")
+    ticket = yf.Ticker(symbol)
+
+    data = ticket.cashflow.transpose()
+    data.index.names = ['Date']
+
+    if is_empty_dir(path):
+        last_update = pd.Timestamp(datetime.datetime(1970, 1, 1, 0, 0))
+        append_mode = False
+    else:
+        os.remove(path+"/_metadata")
+        last_update = pd.read_parquet(
+            path, engine='fastparquet', columns=["Date"])["Date"].max()
+        append_mode = True
+
+    data = data[data.index > last_update.strftime(
+        '%Y-%m-%d')]
+    print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
+    print(f"Number new lines: {str(data.shape[0])}")
+
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(
+        data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
+
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
+        "partition"], append=append_mode)
+
+
+# https://localhost:5000/api/call/cashflow?symbol="MSFT"
+def cashflow(symbol: str = "MSFT") -> pd.core.frame.DataFrame:
+
+    path = f"{DATA_PATH}/cashflow/{symbol}"
+    df_cashflow = pd.read_parquet(path, engine='fastparquet')
+    df_cashflow.set_index("Date", inplace=True)
+
+    df_cashflow.reset_index(drop=False, inplace=True)
+    df_cashflow["Date"] = df_cashflow["Date"].dt.strftime(
+        '%Y-%m-%d')
+    return df_cashflow
+
+
+# https://localhost:5000/api/call/load_quarterly_cashflow?symbol="MSFT"
+def load_quarterly_cashflow(symbol: str = "MSFT"):
+
+    path = f"{DATA_PATH}/quarterly_cashflow/{symbol}"
+    print(f"Loanding quarterly_cashflow : {symbol}")
+    ticket = yf.Ticker(symbol)
+
+    data = ticket.quarterly_cashflow.transpose()
+    data.index.names = ['Date']
+
+    if is_empty_dir(path):
+        last_update = pd.Timestamp(datetime.datetime(1970, 1, 1, 0, 0))
+        append_mode = False
+    else:
+        os.remove(path+"/_metadata")
+        last_update = pd.read_parquet(
+            path, engine='fastparquet', columns=["Date"])["Date"].max()
+        append_mode = True
+
+    data = data[data.index > last_update.strftime(
+        '%Y-%m-%d')]
+    print(f"Last update date: {last_update.strftime('%Y-%m-%d')}")
+    print(f"Number new lines: {str(data.shape[0])}")
+
+    data['partition'] = data.index
+    data['partition'] = pd.Categorical(
+        data['partition'].dt.strftime('%Y-%m'))
+    data.reset_index(drop=False, inplace=True)
+
+    data.to_parquet(path,  engine='fastparquet', partition_cols=[
+        "partition"], append=append_mode)
+
+
+# https://localhost:5000/api/call/quarterly_cashflow?symbol="MSFT"
+def quarterly_cashflow(symbol: str = "MSFT") -> pd.core.frame.DataFrame:
+
+    path = f"{DATA_PATH}/quarterly_cashflow/{symbol}"
+    data = pd.read_parquet(path, engine='fastparquet')
+    data.set_index("Date", inplace=True)
+
+    data.reset_index(drop=False, inplace=True)
+    data["Date"] = data["Date"].dt.strftime(
+        '%Y-%m-%d')
+    return data
 
 
 # load_stock()
@@ -270,28 +360,28 @@ def SP500()-> pd.core.frame.DataFrame:
 
 # load_income()
 # pprint(income())
-#load_quarterly_income()
-#pprint(quarterly_income())
+# load_quarterly_income()
+# pprint(quarterly_income())
 
+if __name__ == '__main__':
 
-for symbol in list(SP500()["Symbol"]):
-    load_stock(symbol)
+    for symbol in list(SP500()["Symbol"]):
+        load_stock(symbol)
+        load_dividends(symbol)
+        load_splits(symbol)
+        load_income(symbol)
+        load_quarterly_income(symbol)
+        load_cashflow(symbol)
+        load_quarterly_cashflow(symbol)
 
-exit(0)
+    exit(0)
 
-msft = yf.Ticker("MSFT")
+    msft = yf.Ticker("MSFT")
+    pprint(msft.calendar)
 
-pprint(msft.calendar)
-
-# TODO: a finir 
-
-# liste du s&p 500 : https://datahub.io/core/s-and-p-500-companies/_r/-/data/constituents.csv
-
-#msft.balance_sheet
-#msft.quarterly_balance_sheet
-## - cash flow statement
-#msft.cashflow
-#msft.quarterly_cashflow
+# TODO: a finir
+# msft.balance_sheet
+# msft.quarterly_balance_sheet
 # msft.get_shares_full(start="2022-01-01", end=None)
 # msft.calendar
 
